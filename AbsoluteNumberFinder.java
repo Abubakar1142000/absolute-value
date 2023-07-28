fae2ff56c83a685669c75c7f5b528ed55f1be9e5
Abubakar1142000 <140464485+Abubakar1142000@users.noreply.github.com>
2023-07-29 02:58:16 +0500

    Create AbsoluteNumberFinder.java

---
+++ AbsoluteNumberFinder.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AbsoluteNumberFinder {
+
+    public static int findAbsoluteValue(int number) {
+        return number < 0 ? -number : number;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter any Integer =");
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int inputNumber = sc.nextInt();
+            int absoluteValue = findAbsoluteValue(inputNumber);
+            System.out.println("The absolute value of " + inputNumber + " is " + absoluteValue);
+        } else {
+            System.out.print("Enter Valid Integer");
+        }
+    }
+}
